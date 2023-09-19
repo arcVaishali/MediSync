e3c3c2b1aacd28f336dc1196fcc99b26be2f3b47
vaishali <vaishalirdj14@gmail.com>
2023-09-19 20:22:04 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Home from "./AppPages/PatientFlow/Home";
+import Signup from "../src/AppPages/Signup";
+import Login from "../src/AppPages/Login";
+import PatientDashboard from "../src/AppPages/PatientDashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<PatientDashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
