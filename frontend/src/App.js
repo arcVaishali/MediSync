0e26fb41f653122ae6dd44792576020dd2aa56d9
vaishali <vaishalirdj14@gmail.com>
2023-09-20 14:27:01 +0530

    login page frontend created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
-import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
 import Home from "./AppPages/PatientFlow/Home";
-import Signup from "../src/AppPages/Signup";
-import Login from "../src/AppPages/Login";
-import PatientDashboard from "../src/AppPages/PatientDashboard";
+import Signup from "./AppPages/PatientFlow/Signup";
+import Login from "./AppPages/PatientFlow/Login";
+import PatientDashboard from "./AppPages/PatientFlow/PatientDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Footer } from "antd/es/layout/layout";
+// import Navbar from "./AppComponents/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
+     {/* <div>
+      <Navbar/>
+     </div> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/home" element={<PatientDashboard />}></Route>
       </Routes>
+      {/* <div> */}
+        {/* <Footer/> */}
+      {/* </div> */}
     </BrowserRouter>
   );
 }
